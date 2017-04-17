d9b9af83a10745538edf82851c88f2876c33263f
Chris Cesare <chris.cesare@gmail.com>
2017-04-17 10:25:15 -0400

    Adding module packaging code for the kicked_rotor graphic

---
+++ kicked_rotor/js/main.js
@@ -0,0 +1,12 @@
+var limiter = $(".graphics-environment").last().parent().attr("id")
+
+var MODULE = (function () {
+
+  var m = {}
+
+  // put everything inside of this function
+  m.init = function () {
+
+  return m;
+
+}());
